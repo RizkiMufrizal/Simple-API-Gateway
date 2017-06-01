b6d1cef5a1a2a6de5ba8421c05dc7af7180c1cf5
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:14:42 +0700

    tambah domain review

---
+++ Book-Gateway/src/main/kotlin/org/rizki/mufrizal/api/gateway/BookGateway/domain/Review.kt
@@ -0,0 +1,7 @@
+package org.rizki.mufrizal.api.gateway.BookGateway.domain
+
+/**
+ * Created by rizkimufrizal on 5/28/17.
+ */
+
+data class Review(val id: Long? = null, val bookId: Long? = null, val reviewText: String? = null)
