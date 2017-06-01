72732310315dadbc0d78688f9b292c8c2925674a
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:09:14 +0700

    initial project

---
+++ Book-Catalog/src/test/kotlin/org/rizki/mufrizal/api/gateway/BookCatalog/BookCatalogApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.api.gateway.BookCatalog
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
