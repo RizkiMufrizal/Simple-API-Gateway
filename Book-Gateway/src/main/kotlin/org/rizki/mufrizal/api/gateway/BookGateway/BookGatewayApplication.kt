f2243c93c7b735319b003590c7667838a635f2e3
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:16:36 +0700

    tambah main spring boot

---
+++ Book-Gateway/src/main/kotlin/org/rizki/mufrizal/api/gateway/BookGateway/BookGatewayApplication.kt
@@ -0,0 +1,11 @@
+package org.rizki.mufrizal.api.gateway.BookGateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookGatewayApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BookGatewayApplication::class.java, *args)
+}
