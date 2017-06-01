3f367a753c31d0c404cbb32327d506d34fc8dc20
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:14:20 +0700

    tambah domain book

---
+++ Book-Gateway/src/main/kotlin/org/rizki/mufrizal/api/gateway/BookGateway/domain/Book.kt
@@ -0,0 +1,7 @@
+package org.rizki.mufrizal.api.gateway.BookGateway.domain
+
+/**
+ * Created by rizkimufrizal on 5/28/17.
+ */
+
+data class Book(val id: Long? = null, val title: String? = null, val description: String? = null)
