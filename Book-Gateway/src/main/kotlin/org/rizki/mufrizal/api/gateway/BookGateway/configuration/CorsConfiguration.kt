a0e1ae417516b9a3b5cb7e648cd83ba7ba1a256e
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:16:59 +0700

    tambah konfigurasi cors

---
+++ Book-Gateway/src/main/kotlin/org/rizki/mufrizal/api/gateway/BookGateway/configuration/CorsConfiguration.kt
@@ -0,0 +1,46 @@
+package org.rizki.mufrizal.api.gateway.BookGateway.configuration
+
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Created by rizkimufrizal on 5/28/17.
+ */
+@Component
+class CorsConfiguration : Filter {
+
+    @Throws(ServletException::class)
+    override fun init(filterConfig: FilterConfig) {
+
+    }
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val httpServletResponse = response as HttpServletResponse
+        val httpServletRequest = request as HttpServletRequest
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*")
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600")
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "accept, authorization, x-requested-with, content-type")
+
+        if (httpServletRequest.method.equals("OPTIONS", ignoreCase = true)) {
+            try {
+                httpServletResponse.status = 200
+                httpServletResponse.writer.print("OK")
+                httpServletResponse.writer.flush()
+            } catch (e: IOException) {
+            }
+
+        } else {
+            chain.doFilter(request, response)
+        }
+    }
+
+    override fun destroy() {
+
+    }
+}
