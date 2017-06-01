3b6f8a93f7dd5ed73669bdd6626a2403fce80bed
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:08:09 +0700

    tambah konfigurasi datasource

---
+++ Book-Review/src/main/kotlin/org/rizki/mufrizal/api/gateway/BookReview/configuration/DataSourceConfiguration.kt
@@ -0,0 +1,36 @@
+package org.rizki.mufrizal.api.gateway.BookReview.configuration
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
+import javax.sql.DataSource
+
+/**
+ * Created by rizkimufrizal on 5/28/17.
+ */
+@Configuration
+@PropertySource("classpath:application.yml")
+class DataSourceConfiguration @Autowired constructor(val environment: Environment) {
+
+    @Bean(destroyMethod = "close")
+    fun dataSource(): DataSource {
+        val dataSourceConfig = HikariConfig()
+        dataSourceConfig.driverClassName = environment.getRequiredProperty("spring.datasource.driver-class-name")
+        dataSourceConfig.jdbcUrl = environment.getRequiredProperty("spring.datasource.url")
+        dataSourceConfig.username = environment.getRequiredProperty("spring.datasource.username")
+        dataSourceConfig.password = environment.getRequiredProperty("spring.datasource.password")
+        dataSourceConfig.maximumPoolSize = environment.getRequiredProperty("spring.datasource.maximumPoolSize").toInt()
+        dataSourceConfig.minimumIdle = environment.getRequiredProperty("spring.datasource.minimumIdle").toInt()
+        dataSourceConfig.connectionTimeout = environment.getRequiredProperty("spring.datasource.connectionTimeout").toLong()
+        dataSourceConfig.idleTimeout = environment.getRequiredProperty("spring.datasource.idleTimeout").toLong()
+        dataSourceConfig.addDataSourceProperty("poolName", environment.getRequiredProperty("spring.datasource.poolName"))
+        dataSourceConfig.addDataSourceProperty("cachePrepStmts", true)
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSize", 250)
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 2048)
+        return HikariDataSource(dataSourceConfig)
+    }
+}
