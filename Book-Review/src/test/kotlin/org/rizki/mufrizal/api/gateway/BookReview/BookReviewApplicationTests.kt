557f7cd3fad8e73724eb9866de396b5df0d2869a
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:04:42 +0700

    initial project

---
+++ Book-Review/src/test/kotlin/org/rizki/mufrizal/api/gateway/BookReview/BookReviewApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.api.gateway.BookReview
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BookReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
