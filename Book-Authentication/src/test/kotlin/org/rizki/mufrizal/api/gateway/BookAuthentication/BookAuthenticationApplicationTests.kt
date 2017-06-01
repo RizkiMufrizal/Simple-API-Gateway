70008b9199d3573986784e8d2374236c94516636
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-06-01 15:00:52 +0700

    initial test

---
+++ Book-Authentication/src/test/kotlin/org/rizki/mufrizal/api/gateway/BookAuthentication/BookAuthenticationApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.api.gateway.BookAuthentication
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BookAuthenticationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
